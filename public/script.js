aa78eb02455b2ca47b5263526ecfd18ec986ddc7
Nestori <nessu.kangashaka@gmail.com>
2023-01-25 17:16:09 +0200

    Start of Part 2

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
 
-const registerSubmit = document.getElementById("submit-input");
+const registerSubmit = document.getElementById("register-submit");
 
 if (registerSubmit !== null)
 {
     registerSubmit.addEventListener("click", () =>
     {
-        const email = document.getElementById("email-input").value;
-        const password = document.getElementById("password-input").value;
+        const registerEmail = document.getElementById("register-email").value;
+        const registerPassword = document.getElementById("register-password").value;
 
-        const body = {
-            email,
-            password
+        const registerBody = {
+            'email': registerEmail,
+            'password': registerPassword
         };
 
         fetch('http://localhost:3000/api/user/register', {
@@ -19,8 +19,38 @@ if (registerSubmit !== null)
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(body)
+        body: JSON.stringify(registerBody)
         })
+        .then(response => response)
         .then(window.location.assign('http://localhost:3000/login.html'));
     })
 }
+
+
+const loginSubmit = document.getElementById("login-submit");
+
+if (loginSubmit !== null)
+{
+    loginSubmit.addEventListener("click", () =>
+    {
+        const loginEmail = document.getElementById("login-email").value;
+        const loginPassword = document.getElementById("login-password").value;
+
+        const loginBody = {
+            'email': loginEmail,
+            'password': loginPassword
+        };
+
+        fetch('http://localhost:3000/api/user/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginBody)
+        })
+        .then(response => console.log(response))
+        .then(window.location.assign('http://localhost:3000/'));
+    });
+}
+
